7f69160f8e9f8141239f5159dce50f55edfeb34e
Kashif1122666 <158280129+Kashif1122666@users.noreply.github.com>
2024-07-26 14:45:16 -0700

    Add files via upload
    Rounding-numbers-js

---
+++ Rounding-numbers-js/index.js
@@ -0,0 +1,62 @@
+// Chapter 26: Rounding numbers
+
+// module we're gonna talk about
+// Math
+
+// methods we're gonna talk about
+// round() -> it rounds a number to nearest integer.
+// ceil() -> it rounds a number up to nearest integer.
+// floor() -> it rounds a number down to nearest integer.
+
+// const percentage = 73.56;
+// console.log(Math.round(percentage));
+
+// round
+// const a = Math.round(2.6);
+// console.log(a);
+// const b = Math.round(2.5);
+// console.log(b);
+// const c = Math.round(2.49);
+// console.log(c);
+
+// negetive numbers  such as -1 ,-2, -3 etc., are considered "down" values and tend to 
+// round down .when rounding negetive numbers , the rounding is done downwards, which means negetive numbers are 
+// rounded down. 
+
+// const d = Math.round(-2.6);
+// console.log(d);
+// const e = Math.round(-2.5);
+// console.log(e);
+// const f = Math.round(-2.49);
+// console.log(f);
+
+// ceil 
+// console.log(Math.ceil(1.4));
+// const a = Math.ceil(0.6);
+// console.log(a);
+// const b = Math.ceil(0.4);
+// console.log(b);
+// const c = Math.ceil(5);
+// console.log(c);
+// const d = Math.ceil(5.1);
+// console.log(d);
+// const e = Math.ceil(-5.1);
+// console.log(e);
+// const f = Math.ceil(-5.9);
+// console.log(f);
+
+// // floor
+// console.log(Math.floor(1.6));
+// console.log(Math.floor(1.4));
+// const a = Math.floor(0.6);
+// console.log(a);
+// const b = Math.floor(0.4);
+// console.log(b);
+// const c = Math.floor(5);
+// console.log(c);
+// const d = Math.floor(5.1);
+// console.log(d);
+// const e = Math.floor(-5.1);
+// console.log(e);
+// const f = Math.floor(-5.9);
+// console.log(f);
